7cb3cd86529b0fb2d8c16ce29eb8d1037a3196b0
FSRangelF <fs.rangel@gmail.com>
2025-10-31 11:06:17 -0300

    implemented main logic function.

--- main.py
+++ main.py
@@ -17,38 +17,12 @@ def print_report():
     print(f"    Coffee: {current_resources["coffee"]}")
     print(f"    Money: {current_resources["money"]}") 
 
-while not should_stop:
-    print(art.logo)
-# 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-    # a. Check the user’s input to decide what to do next.
-    valid_option = False
-    while not valid_option:
-        option = input("What would you like? (espresso/latte/cappuccino): ").lower()
-        if option == "off":
-# 2. Turn off the Coffee Machine by entering “off” to the prompt. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. Your code should end execution when this happens.
-            valid_option = True
-            should_stop = True
-            print("Good Bye!")
-        elif option == "report":
-            valid_option = True
-            print_report()
-        else:
-            print("You typed an invalid option. Please choose again.")
-
-    # 1b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer
-
-
-
-
-# 4. Check resources sufficient?
-    # a. When the user chooses a drink, the program should check if there are enough resources to make that drink.
-    # b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make the drink but print: “Sorry there is not enough water.”
-    # c. The same should happen if another resource is depleted, e.g. milk or coffee.
-
 # 5. Process coins.
     # a. If there are sufficient resources to make the drink selected, then the program should prompt the user to insert coins.
     # b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
     # c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2 pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+def prompt_payment():
+    pass
 
 # 6. Check transaction successful?
     # a. Check that the user has inserted enough money to purchase the drink they selected. E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the program should say “Sorry that's not enough money. Money refunded.”.
@@ -58,17 +32,53 @@ while not should_stop:
         # Coffee: 76g
         # Money: $2.5
     # c. If the user has inserted too much money, the machine should offer change. E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
+def transaction_successful():
+    return True
 
-# 7. Make Coffee.
-    # a. If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the coffee machine resources.
-    # E.g. report before purchasing latte:
-        # Water: 300ml
-        # Milk: 200ml
-        # Coffee: 100g
-        # Money: $0
-    # Report after purchasing latte:
-        # Water: 100ml
-        # Milk: 50ml
-        # Coffee: 76g
-        # Money: $2.5
-    # b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink.
+    # 7. Make Coffee.
+        # a. If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the coffee machine resources.
+        # E.g. report before purchasing latte:
+            # Water: 300ml
+            # Milk: 200ml
+            # Coffee: 100g
+            # Money: $0
+        # Report after purchasing latte:
+            # Water: 100ml
+            # Milk: 50ml
+            # Coffee: 76g
+            # Money: $2.5
+        # b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink.
+def make_coffee(menu_option):
+    print("Here is your latte. Enjoy!")
+
+# 4. Check resources sufficient?
+    # a. When the user chooses a drink, the program should check if there are enough resources to make that drink.
+    # b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make the drink but print: “Sorry there is not enough water.”
+    # c. The same should happen if another resource is depleted, e.g. milk or coffee.
+def suficient_resources(menu_option):
+    return True
+
+while not should_stop:
+    print(art.logo)
+    # 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+        # a. Check the user’s input to decide what to do next.
+        # b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer
+    valid_option = False
+    while not valid_option:
+        option = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        # 2. Turn off the Coffee Machine by entering “off” to the prompt. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. Your code should end execution when this happens.
+        if option == "off":
+            valid_option = True
+            should_stop = True
+            print("Good Bye!")
+        elif option == "report":
+            valid_option = True
+            print_report()
+        elif option in resources.MENU:
+            valid_option = True
+            if suficient_resources:
+                prompt_payment()
+                if transaction_successful():
+                    make_coffee(option)
+        else:
+            print("You typed an invalid option. Please choose again.")
