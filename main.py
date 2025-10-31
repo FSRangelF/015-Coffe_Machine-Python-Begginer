25f4a1dae35133ddd9fa119647c7ed90b8fe6311
FSRangelF <fs.rangel@gmail.com>
2025-10-31 20:46:33 -0300

    added prompt_payment() function

--- main.py
+++ main.py
@@ -22,8 +22,24 @@ def print_report():
     # b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
     # c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2 pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
 def prompt_payment():
-    pass
-
+    try:
+        quarter = int(input("How many quarters you want to insert: "))
+    except ValueError:
+        quarter = 0
+    try:
+        dime = int(input("How many dimes you want to insert: "))
+    except ValueError:
+        dime = 0
+    try:
+        nickel = int(input("How many nickels you want to insert: "))
+    except ValueError:
+        nickel = 0
+    try:
+        pennie = int(input("How many pennies you want to insert: "))
+    except ValueError:
+        pennie = 0
+    return quarter*0.25 + dime*0.1 + nickel*0.05 + pennie*0.01
+  
 # 6. Check transaction successful?
     # a. Check that the user has inserted enough money to purchase the drink they selected. E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the program should say “Sorry that's not enough money. Money refunded.”.
     # b. But if the user has inserted enough money, then the cost of the drink gets added to the machine as the profit and this will be reflected the next time “report” is triggered. E.g.
@@ -32,7 +48,8 @@ def prompt_payment():
         # Coffee: 76g
         # Money: $2.5
     # c. If the user has inserted too much money, the machine should offer change. E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
-def transaction_successful():
+def transaction_successful(payment, menu_option):
+    
     return True
 
     # 7. Make Coffee.
@@ -88,8 +105,9 @@ while not should_stop:
         elif option in resources.MENU:
             valid_option = True
             if suficient_resources(option):
-                prompt_payment()
-                if transaction_successful():
+                money_amount = prompt_payment()
+                print(money_amount)
+                if transaction_successful(money_amount, option):
                     make_coffee(option)
         else:
             print("You typed an invalid option. Please choose again.")
